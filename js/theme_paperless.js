a54bd67c78f3b5e166dde9464ed1c10519747210
Rajitha <denzelrajitha@gmail.com>
2020-10-27 16:16:35 +0530

    accordion function

--- js/theme_paperless.js
+++ js/theme_paperless.js
@@ -8,3 +8,11 @@ menuBtn.addEventListener("click", menuControl);
 function menuControl() {
     menu.classList.toggle('open');
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+    });
+}
